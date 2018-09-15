cfb62b025c65c5146c3489745a0e84e3dc9932aa
Itay BS <Taytay884@yahoo.com>
2018-09-15 09:54:10 +0300

    Added BurgerBuilder Component and styling to layout

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 
+import Layout from './components/Layout/Layout';
+import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
+
 class App extends Component {
   render() {
     return (
       <div>
-        <h1>The spectacle before us was indeed sublime.</h1>
-        <p>Hey, What's up everybody?!</p>
+        <Layout>
+          <BurgerBuilder />
+        </Layout>
       </div>
     );
   }
